3d2a076b3863bc154e6275ea38310805395bfcdd
MCydia <61176750+MCydia@users.noreply.github.com>
2021-02-21 19:24:50 +0800

    Update diy.sh

---
+++ diy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+# Modify default IP
+#sed -i 's/10.10.10.251/10.10.10.251/g' package/base-files/files/bin/config_generate
+pwd
+ls -l
+echo "开始 DIY 配置"
+pwd
+ls -l
