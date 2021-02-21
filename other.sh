b1237572056bbc3ec5e223c6a8085346fdbc8d6e
MCydia <61176750+MCydia@users.noreply.github.com>
2021-02-21 19:25:32 +0800

    Update bypass

---
+++ other.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+# Modify default IP
+#sed -i 's/10.10.10.251/10.10.10.251/g' package/base-files/files/bin/config_generate
+pwd
+ls -l
+echo "开始 DIY 配置"
+pwd
+ls -l
+
+cd openwrt
+
+find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
+find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
