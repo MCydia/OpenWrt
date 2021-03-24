6526027fccb9483d2faba394ae36de0d69d8c42d
MCydia <61176750+MCydia@users.noreply.github.com>
2021-03-24 15:45:22 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -14,7 +14,7 @@ sed -i "/uci commit fstab/a\uci commit network" $ZZZ
 sed -i "/uci commit network/i\uci set network.lan.ipaddr='10.10.10.251'" $ZZZ             # IPv4 地址(openwrt后台地址)
 sed -i "/uci commit network/i\uci set network.lan.netmask='255.255.255.0'" $ZZZ           # IPv4 子网掩码
 sed -i "/uci commit network/i\uci set network.lan.gateway='10.10.10.250'" $ZZZ            # IPv4 网关
-sed -i "/uci commit network/i\uci set network.lan.broadcast='192.168.2.255'" $ZZZ         # IPv4 广播
+sed -i "/uci commit network/i\uci set network.lan.broadcast='10.10.10.255'" $ZZZ          # IPv4 广播
 sed -i "/uci commit network/i\uci set network.lan.dns='10.10.10.253'" $ZZZ                # DNS(多个DNS要用空格分开)
 sed -i "/uci commit network/i\uci set network.lan.delegate='0'" $ZZZ                      # 去掉LAN口使用内置的 IPv6 管理
 
