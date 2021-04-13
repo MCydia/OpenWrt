57879f929aca6220c32ddf0ec5153b1e80bd39c3
MCydia <61176750+MCydia@users.noreply.github.com>
2021-04-13 16:37:32 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,16 +6,9 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/10.10.10.251/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.10.10.251/g' package/base-files/files/bin/config_generate
 
 ZZZ="package/lean/default-settings/files/zzz-default-settings"
-sed -i "/uci commit fstab/a\uci commit network" $ZZZ
-sed -i "/uci commit network/i\uci set network.lan.ipaddr='10.10.10.251'" $ZZZ             # IPv4 地址(openwrt后台地址)
-sed -i "/uci commit network/i\uci set network.lan.netmask='255.255.255.0'" $ZZZ           # IPv4 子网掩码
-sed -i "/uci commit network/i\uci set network.lan.gateway='10.10.10.250'" $ZZZ            # IPv4 网关
-sed -i "/uci commit network/i\uci set network.lan.broadcast='0'" $ZZZ                     # IPv4 广播
-sed -i "/uci commit network/i\uci set network.lan.dns='10.10.10.253'" $ZZZ                # DNS(多个DNS要用空格分开)
-sed -i "/uci commit network/i\uci set network.lan.delegate='0'" $ZZZ                      # 去掉LAN口使用内置的 IPv6 管理
 
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile  # 强制选择argon为默认主题选项
 #sed -i "/uci commit luci/i\uci set luci.main.mediaurlbase=/luci-static/argon" $ZZZ       # 选择argon为默认主题
