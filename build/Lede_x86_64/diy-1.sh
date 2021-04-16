dd500c681688352d795b8692e549e36d06576559
MCydia <61176750+MCydia@users.noreply.github.com>
2021-04-16 19:04:57 +0800

    up

--- build/Lede_x86_64/diy-1.sh
+++ build/Lede_x86_64/diy-1.sh
@@ -5,7 +5,7 @@
 # 要了解增加了什么东西，就到我的专用软件包里面看看看吧，如果还是没有你需要的插件，请不要一下子就拉取别人的插件包
 # 相同的文件都拉一起，因为有一些可能还是其他大神修改过的容易造成编译错误的
 # 想要什么插件就单独的拉取什么插件就好，或者告诉我，我把插件放我的插件包就行了
-# 软件包地址：https://github.com/281677160/openwrt-package
+# 软件包地址：https://github.com/longcat99/long.git
 # 拉取插件请看《各种命令的简单介绍》第4条、第5条说明,不管大神还是新手请认真的看看,再次强调请不要一下子就拉取别人一堆插件的插件包,容易造成编译错误的
 cd package
 git clone https://github.com/longcat99/long.git
@@ -36,3 +36,10 @@ git clone --depth 1 https://github.com/sirpdboy/luci-app-autotimeset
 git clone --depth 1 https://github.com/sirpdboy/luci-theme-opentoks
 # svn co https://github.com/sirpdboy/netspeedtest/trunk/luci-app-NetSpeedTest
 # svn co https://github.com/sirpdboy/netspeedtest/trunk/speedtest
+#atmaterial主题
+git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git
+#opentomato主题
+svn co https://github.com/solidus1983/luci-theme-opentomato/trunk/luci/themes/luci-theme-opentomato
+# netdata
+rm -rf luci-app-netdata && git clone https://github.com/sirpdboy/luci-app-netdata.git
+
