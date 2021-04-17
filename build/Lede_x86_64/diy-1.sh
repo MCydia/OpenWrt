d30302dd777fcbf55928131bd9cc2a82ef140909
MCydia <61176750+MCydia@users.noreply.github.com>
2021-04-18 04:05:10 +0800

    Update diy-1.sh

--- build/Lede_x86_64/diy-1.sh
+++ build/Lede_x86_64/diy-1.sh
@@ -7,10 +7,6 @@
 # 想要什么插件就单独的拉取什么插件就好，或者告诉我，我把插件放我的插件包就行了
 # 软件包地址：https://github.com/281677160/openwrt-package.git
 # 拉取插件请看《各种命令的简单介绍》第4条、第5条说明,不管大神还是新手请认真的看看,再次强调请不要一下子就拉取别人一堆插件的插件包,容易造成编译错误的
-cd package
-git clone https://github.com/281677160/openwrt-package.git
-mkdir openwrt-packages
-cd openwrt-packages
 git clone https://github.com/frainzy1477/luci-app-clash.git
 git clone https://github.com/destan19/OpenAppFilter.git
 git clone https://github.com/jerrykuku/luci-app-ttnode.git
@@ -42,4 +38,3 @@ git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git
 svn co https://github.com/solidus1983/luci-theme-opentomato/trunk/luci/themes/luci-theme-opentomato
 # netdata
 rm -rf luci-app-netdata && git clone https://github.com/sirpdboy/luci-app-netdata.git
-
