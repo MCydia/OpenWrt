c6a98a35acea4d2681887cbf7bbac4d7e1e8a8f6
mrchan26 <50740155+mrchan26@users.noreply.github.com>
2021-04-18 14:44:18 +0800

    Update diy-2.sh

--- build/Lienol_x86_64/diy-2.sh
+++ build/Lienol_x86_64/diy-2.sh
@@ -4,10 +4,10 @@ ZZZ="package/default-settings/files/zzz-default-settings"
 #
 
 sed -i "/uci commit fstab/a\uci commit network" $ZZZ
-sed -i "/uci commit network/i\uci set network.lan.ipaddr='192.168.2.2'" $ZZZ              # IPv4 地址(openwrt后台地址)
+sed -i "/uci commit network/i\uci set network.lan.ipaddr='10.10.10.12'" $ZZZ              # IPv4 地址(openwrt后台地址)
 sed -i "/uci commit network/i\uci set network.lan.netmask='255.255.255.0'" $ZZZ           # IPv4 子网掩码
-sed -i "/uci commit network/i\uci set network.lan.gateway='192.168.2.1'" $ZZZ             # IPv4 网关
-sed -i "/uci commit network/i\uci set network.lan.broadcast='192.168.2.255'" $ZZZ         # IPv4 广播
+sed -i "/uci commit network/i\uci set network.lan.gateway='10.10.10.10'" $ZZZ             # IPv4 网关
+sed -i "/uci commit network/i\uci set network.lan.broadcast='10.10.10.255'" $ZZZ          # IPv4 广播
 sed -i "/uci commit network/i\uci set network.lan.dns='223.5.5.5 114.114.114.114'" $ZZZ   # DNS(多个DNS要用空格分开)
 sed -i "/uci commit network/i\uci set network.lan.delegate='0'" $ZZZ                      # 去掉LAN口使用内置的 IPv6 管理
 
@@ -16,7 +16,7 @@ sed -i "/uci commit luci/i\uci set luci.main.mediaurlbase=/luci-static/argon" $Z
 
 sed -i "/uci commit system/i\uci set system.@system[0].hostname='OpenWrt-123'" $ZZZ       # 修改主机名称为OpenWrt-123
 
-sed -i "s/OpenWrt /281677160 build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ        # 增加自己个性名称281677160
+sed -i "s/OpenWrt /JasonFreedom build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ     # 增加自己个性名称
 
 #sed -i 's/PATCHVER:=4.14/PATCHVER:=4.19/g' target/linux/x86/Makefile                     # 修改内核版本为4.19
 
