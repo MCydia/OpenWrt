5cc0715fa2b4c18254851cb623695405c5603280
MCydia <61176750+MCydia@users.noreply.github.com>
2021-04-27 01:40:27 +0800

    Update diy-1.sh

--- build/Lede_x86_64/diy-1.sh
+++ build/Lede_x86_64/diy-1.sh
@@ -8,8 +8,32 @@
 # 软件包地址：https://github.com/MCydia/openwrt-package.git
 # 再次强调请不要一下子就拉取别人一堆插件的插件包,容易造成编译错误的
 
-# cd openwrt-packages
-# Lua-Maxminddb:
-# svn co https://github.com/garypang13/openwrt-packages/trunk/lua-maxminddb
-# smartdns-le:
-# git clone https://github.com/garypang13/smartdns-le
+cd package
+git clone https://github.com/MCydia/openwrt-package.git
+mkdir openwrt-packages
+cd openwrt-packages
+git clone https://github.com/frainzy1477/luci-app-clash.git
+git clone https://github.com/destan19/OpenAppFilter.git
+git clone https://github.com/jerrykuku/luci-app-ttnode.git
+git clone https://github.com/linkease/ddnsto-openwrt.git
+git clone https://github.com/lisaac/luci-app-dockerman.git
+git clone https://github.com/lisaac/luci-lib-docker.git
+git clone https://github.com/longcat99/luci-app-dnsfilter.git
+git clone --depth 1 https://github.com/garypang13/smartdns-le
+git clone https://github.com/kuoruan/luci-app-kcptun.git
+git clone https://github.com/project-lede/luci-app-godproxy.git
+svn co https://github.com/garypang13/openwrt-packages/trunk/lua-maxminddb
+git clone https://github.com/riverscn/openwrt-iptvhelper.git && mv openwrt-iptvhelper/luci-app-iptvhelper/po/zh_Hans openwrt-iptvhelper/luci-app-iptvhelper/po/zh-cn
+svn co https://github.com/Lienol/openwrt/trunk/package/network/fullconenat && rm -f fullconenat/patches/000-printk.patch        
+git clone --depth 1 -b LUCI-LUA-UCITRACK https://github.com/CCnut/feed-netkeeper && mv -n feed-netkeeper/* ./ ; rm -Rf feed-netkeeper
+git clone https://github.com/immortalwrt/openwrt-gowebdav
+git clone --depth 1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config
+git clone https://github.com/garypang13/luci-app-eqos.git
+# git clone --depth 1 -b 18.06 https://github.com/riverscn/luci-app-omcproxy.git
+git clone --depth 1 https://github.com/esirplayground/luci-app-LingTiGameAcc
+git clone --depth 1 https://github.com/esirplayground/LingTiGameAcc
+git clone --depth 1 https://github.com/sirpdboy/luci-app-autotimeset
+git clone --depth 1 https://github.com/sirpdboy/luci-theme-opentoks
+# svn co https://github.com/sirpdboy/netspeedtest/trunk/luci-app-NetSpeedTest
+# svn co https://github.com/sirpdboy/netspeedtest/trunk/speedtest
