b0b1504ac5e1fc9552dd3e6354ef2db1c26fc43b
MCydia <61176750+MCydia@users.noreply.github.com>
2021-05-19 18:59:54 +0800

    Update webweb.sh

--- build/common/PROJECT/files/etc/webweb.sh
+++ build/common/PROJECT/files/etc/webweb.sh
@@ -3,12 +3,17 @@
 if [ -n "$(ls -A "/etc/openwrt_gxqm" 2>/dev/null)" ]; then
   Opgxqm="$(awk 'NR==1' /etc/openwrt_gxqm)"
   sed -i '/DESCRIPTION/d' /etc/openwrt_release
-  echo "DISTRIB_DESCRIPTION='"${Opgxqm}" @ ImmortalWrt 18.06-SNAPSHOT'" >> /etc/openwrt_release
+  echo "DISTRIB_DESCRIPTION='"${Opgxqm}" @ ImmortalWrt 21.02-SNAPSHOT'" >> /etc/openwrt_release
   rm -rf /etc/openwrt_gxqm
 fi
-sed -i 's/<%=pcdata(ver.distversion)%>/<%=pcdata(ver.distversion)%><!--/g' /usr/lib/lua/luci/view/admin_status/index.htm
-sed -i 's/(<%=pcdata(ver.luciversion)%>)/(<%=pcdata(ver.luciversion)%>)-->/g' /usr/lib/lua/luci/view/admin_status/index.htm
-sed -i '/github.com/d' /usr/lib/lua/luci/view/admin_status/index.htm
+if [ -n "$(ls -A "/etc/closedhcp" 2>/dev/null)" ]; then
+  sed -i "s/option start '100'/option ignore '1'/g" /etc/config/dhcp
+  sed -i '/limit/d' /etc/config/dhcp
+  sed -i '/leasetime/d' /etc/config/dhcp
+  rm -rf /etc/closedhcp
+fi
 sed -i '/coremark.sh/d' /etc/crontabs/root
-sed -i '/webweb.sh/d' /etc/crontabs/root
+sed -i '/luciname/d' /usr/lib/lua/luci/version.lua
+sed -i '/luciversion/d' /usr/lib/lua/luci/version.lua
 rm -rf /etc/webweb.sh
+
