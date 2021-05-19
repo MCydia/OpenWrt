afbc7523ff84c38e866dd2fe70c3264da4aa6797
MCydia <61176750+MCydia@users.noreply.github.com>
2021-05-19 18:58:03 +0800

    Update webweb.sh

--- build/common/LEDE/files/etc/webweb.sh
+++ build/common/LEDE/files/etc/webweb.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-sed -i 's/<%=pcdata(ver.distversion)%>/<%=pcdata(ver.distversion)%><!--/g' /usr/lib/lua/luci/view/admin_status/index.htm
-sed -i 's/(<%=pcdata(ver.luciversion)%>)/(<%=pcdata(ver.luciversion)%>)-->/g' /usr/lib/lua/luci/view/admin_status/index.htm
-sed -i '/webweb.sh/d' /etc/crontabs/root
+sed -i '/coremark.sh/d' /etc/crontabs/root 2>/dev/null
+if [ -n "$(ls -A "/etc/closedhcp" 2>/dev/null)" ]; then
+  sed -i "s/option start '100'/option ignore '1'/g" /etc/config/dhcp
+  sed -i '/limit/d' /etc/config/dhcp
+  sed -i '/leasetime/d' /etc/config/dhcp
+  rm -rf /etc/closedhcp
+fi
+sed -i '/luciname/d' /usr/lib/lua/luci/version.lua
+sed -i '/luciversion/d' /usr/lib/lua/luci/version.lua
+chmod +x /etc/webluci
+echo "*/4 * * * * source /etc/webluci >/dev/null 2>&1" >> /etc/crontabs/root
 rm -rf /etc/webweb.sh
