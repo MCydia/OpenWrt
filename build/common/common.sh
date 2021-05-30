39fc7c33fd51003fad481754dac8c4f64704bce5
MCydia <61176750+MCydia@users.noreply.github.com>
2021-05-30 19:38:46 +0800

    Update common.sh

--- build/common/common.sh
+++ build/common/common.sh
@@ -14,7 +14,7 @@ IMZZZ="package/emortal/default-settings/files/zzz-default-settings"
 ################################################################################################################
 Diy_all() {
 DIY_GET_COMMON_SH
-echo -e "\nsrc-git mcydia https://github.com/MCydia/openwrt-package.git;$REPO_BRANCH" >> feeds.conf.default
+echo -e "\nsrc-git mcydia https://github.com/MCydia/openwrt-package.git;$REPO_BRANCH" >> feeds.config
 mv "${PATH1}"/AutoBuild_Tools.sh package/base-files/files/bin
 chmod +x package/base-files/files/bin/AutoBuild_Tools.sh
 if [[ ${REGULAR_UPDATE} == "true" ]]; then
