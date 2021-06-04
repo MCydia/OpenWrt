64c222c991fa67a88a9c858fb5190c670bdf37f2
MCydia <61176750+MCydia@users.noreply.github.com>
2021-06-04 14:21:14 +0800

    Update upgrade.sh

--- build/common/upgrade.sh
+++ build/common/upgrade.sh
@@ -4,15 +4,9 @@
 # AutoBuild Functions
 
 GET_TARGET_INFO() {
+        TARGET_BOARD="$(awk -F '[="]+' '/TARGET_BOARD/{print $2}' .config)"
+        TARGET_SUBTARGET="$(awk -F '[="]+' '/TARGET_SUBTARGET/{print $2}' .config)"
 	[ -f ${GITHUB_WORKSPACE}/Openwrt.info ] && . ${GITHUB_WORKSPACE}/Openwrt.info
-	TARGET_BOARD="$(awk -F '[="]+' '/TARGET_BOARD/{print $2}' .config)"
-	TARGET_SUBTARGET="$(awk -F '[="]+' '/TARGET_SUBTARGET/{print $2}' .config)"
-	if [[ "${TARGET_BOARD}" == "x86" ]];then
-		TARGET_PROFILE="x86-64"
-	else
-		TARGET_PROFILE="$(egrep -o "CONFIG_TARGET.*DEVICE.*=y" .config | sed -r 's/.*DEVICE_(.*)=y/\1/')"
-	fi
-	[[ -z "${TARGET_PROFILE}" ]] && TARGET_PROFILE="Unknown"
 	case "${TARGET_PROFILE}" in
 	x86-64)
 		if [ `grep -c "CONFIG_TARGET_IMAGES_GZIP=y" ${Home}/.config` -eq '1' ]; then
@@ -22,10 +16,11 @@ GET_TARGET_INFO() {
 		fi
 	;;
 	esac
-	case "${REPO_URL}" in
-	"${LEDE}")
+	case "${REPO_BRANCH}" in
+	"master")
 		COMP1="coolsnowwolf-18.06"
 		COMP2="lede"
+		ZUOZHE="Lean's"
 		if [[ "${TARGET_PROFILE}" == "x86-64" ]]; then
 			Up_Firmware="openwrt-x86-64-generic-squashfs-combined.${Firmware_sfxo}"
 			EFI_Up_Firmware="openwrt-x86-64-generic-squashfs-combined-efi.${Firmware_sfxo}"
@@ -41,9 +36,10 @@ GET_TARGET_INFO() {
 			Firmware_sfx="${Extension}"
 		fi
 	;;
-	"${LIENOL}") 
+	"19.07") 
 		COMP1="openwrt-19.07"
 		COMP2="lienol"
+		ZUOZHE="Lienol's"
 		if [[ "${TARGET_PROFILE}" == "x86-64" ]]; then
 			Up_Firmware="openwrt-x86-64-combined-squashfs.${Firmware_sfxo}"
 			EFI_Up_Firmware="openwrt-x86-64-combined-squashfs-efi.${Firmware_sfxo}"
@@ -59,9 +55,29 @@ GET_TARGET_INFO() {
 			Firmware_sfx="${Extension}"
 		fi
 	;;
-	"${PROJECT}")
-		COMP1="immortalwrt-21.02"
+	"openwrt-18.06")
+		COMP1="immortalwrt-18.06"
 		COMP2="project"
+		ZUOZHE="ctcgfw"
+		if [[ "${TARGET_PROFILE}" == "x86-64" ]]; then
+			Up_Firmware="immortalwrt-x86-64-combined-squashfs.${Firmware_sfxo}"
+			EFI_Up_Firmware="immortalwrt-x86-64-uefi-gpt-squashfs.${Firmware_sfxo}"
+			Firmware_sfx="${Firmware_sfxo}"
+		elif [[ "${TARGET_PROFILE}" == "phicomm-k3" ]]; then
+			Up_Firmware="immortalwrt-bcm53xx-phicomm-k3-squashfs.trx"
+			Firmware_sfx="trx"
+		elif [[ "${TARGET_PROFILE}" =~ (d-team_newifi-d2|phicomm_k2p) ]]; then
+			Up_Firmware="immortalwrt-${TARGET_BOARD}-${TARGET_SUBTARGET}-${TARGET_PROFILE}-squashfs-sysupgrade.bin"
+			Firmware_sfx="bin"
+		else
+			Up_Firmware="${Updete_firmware}"
+			Firmware_sfx="${Extension}"
+		fi
+	;;
+	"openwrt-21.02")
+		COMP1="ctcgfw-21.02"
+		COMP2="Spirit"
+		ZUOZHE="ctcgfw"
 		if [[ "${TARGET_PROFILE}" == "x86-64" ]]; then
 			Up_Firmware="immortalwrt-x86-64-generic-squashfs-combined.${Firmware_sfxo}"
 			EFI_Up_Firmware="immortalwrt-x86-64-generic-squashfs-combined-efi.${Firmware_sfxo}"
@@ -80,9 +96,12 @@ GET_TARGET_INFO() {
 	esac
 	if [[ ${REGULAR_UPDATE} == "true" ]]; then
 		AutoUpdate_Version=$(awk 'NR==6' package/base-files/files/bin/AutoUpdate.sh | awk -F '[="]+' '/Version/{print $2}')
+		[[ -z "${AutoUpdate_Version}" ]] && AutoUpdate_Version="Unknown"
 	fi
-	User_Repo="$(grep "https://github.com/[a-zA-Z0-9]" ${GITHUB_WORKSPACE}/.git/config | cut -c8-100 | sed 's/^[ \t]*//g')"
-	Github_UP_RELEASE="${GITURL}/releases/update_Firmware"
+	Github_Release="${Github}/releases/download/AutoUpdate"
+	Github_Tags="https://api.github.com/repos/${Apidz}/releases/tags/AutoUpdate"
+	XiaZai="${Apidz}/releases/download/AutoUpdate"
+	Github_UP_RELEASE="${Github}/releases/AutoUpdate"
 	In_Firmware_Info="package/base-files/files/etc/openwrt_info"
 	Openwrt_Version="${COMP2}-${TARGET_PROFILE}-${Compile_Date}"
 }
@@ -96,15 +115,18 @@ Diy_Part1() {
 
 Diy_Part2() {
 	GET_TARGET_INFO
-	[[ -z "${AutoUpdate_Version}" ]] && AutoUpdate_Version="Unknown"
-	[[ -z "${Author}" ]] && Author="Unknown"
-	echo "Github=${User_Repo}" > ${In_Firmware_Info}
+	echo "Github=${Github}" > ${In_Firmware_Info}
+	echo "Author=${Author}" >> ${In_Firmware_Info}
+	echo "CangKu=${CangKu}" >> ${In_Firmware_Info}
+	echo "Luci_Edition=${OpenWrt_name}" >> ${In_Firmware_Info}
 	echo "CURRENT_Version=${Openwrt_Version}" >> ${In_Firmware_Info}
 	echo "DEFAULT_Device=${TARGET_PROFILE}" >> ${In_Firmware_Info}
 	echo "Firmware_Type=${Firmware_sfx}" >> ${In_Firmware_Info}
 	echo "Firmware_COMP1=${COMP1}" >> ${In_Firmware_Info}
 	echo "Firmware_COMP2=${COMP2}" >> ${In_Firmware_Info}
-	
+	echo "Github_Release=${Github_Release}" >> ${In_Firmware_Info}
+	echo "Github_Tags=${Github_Tags}" >> ${In_Firmware_Info}
+	echo "XiaZai=${XiaZai}" >> ${In_Firmware_Info}
 }
 
 Diy_Part3() {
