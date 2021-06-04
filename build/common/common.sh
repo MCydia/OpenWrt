ad6a06fea1aa0ca0be924e201499df52b44c3de3
MCydia <61176750+MCydia@users.noreply.github.com>
2021-06-04 18:01:19 +0800

    Update common.sh

--- build/common/common.sh
+++ build/common/common.sh
@@ -44,7 +44,6 @@ git clone https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwal
 git clone https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
 svn co  https://github.com/vernesong/OpenClash/trunk feeds/luci/luci-app-openclash > /dev/null 2>&1
 git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash
-git clone https://github.com/garypang13/luci-app-bypass package/luci-app-bypass
 
 sed -i "/exit 0/i\chmod +x /etc/webweb.sh && source /etc/webweb.sh > /dev/null 2>&1" package/base-files/files/etc/rc.local
 }
@@ -62,7 +61,6 @@ git clone https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwal
 git clone https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
 svn co  https://github.com/vernesong/OpenClash/trunk feeds/luci/luci-app-openclash > /dev/null 2>&1
 git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash
-git clone https://github.com/garypang13/luci-app-bypass package/luci-app-bypass
 
 curl -fsSL https://raw.githubusercontent.com/281677160/AdGuardHome/main/luci-app-adguardhome/root/etc/config/AdGuardHome.yaml > package/diy/luci-app-adguardhome/root/etc/config/AdGuardHome.yaml
 curl -fsSL https://raw.githubusercontent.com/281677160/AdGuardHome/main/luci-app-adguardhome/po/zh-cn/AdGuardHome.po > package/diy/luci-app-adguardhome/po/zh-cn/AdGuardHome.po
