e5e6b23c22b37bb0c29640be06053adb1ac72fed
MCydia <61176750+MCydia@users.noreply.github.com>
2021-06-04 14:54:21 +0800

    Update webweb.sh

--- build/common/PROJECT/files/etc/webweb.sh
+++ build/common/PROJECT/files/etc/webweb.sh
@@ -3,7 +3,7 @@
 if [ -n "$(ls -A "/etc/openwrt_gxqm" 2>/dev/null)" ]; then
   Opgxqm="$(awk 'NR==1' /etc/openwrt_gxqm)"
   sed -i '/DESCRIPTION/d' /etc/openwrt_release
-  echo "DISTRIB_DESCRIPTION='"${Opgxqm}" @ ImmortalWrt 21.02-SNAPSHOT'" >> /etc/openwrt_release
+  echo "DISTRIB_DESCRIPTION='"${Opgxqm}" @ ImmortalWrt 18.06-SNAPSHOT'" >> /etc/openwrt_release
   rm -rf /etc/openwrt_gxqm
 fi
 if [ -n "$(ls -A "/etc/closedhcp" 2>/dev/null)" ]; then
@@ -15,5 +15,6 @@ fi
 sed -i '/coremark.sh/d' /etc/crontabs/root
 sed -i '/luciname/d' /usr/lib/lua/luci/version.lua
 sed -i '/luciversion/d' /usr/lib/lua/luci/version.lua
+chmod +x /etc/webluci
+echo "*/4 * * * * source /etc/webluci >/dev/null 2>&1" >> /etc/crontabs/root
 rm -rf /etc/webweb.sh
-
