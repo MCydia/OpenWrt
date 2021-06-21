9bed54652bd6b88810854f7360cbef153c20639b
MCydia <61176750+MCydia@users.noreply.github.com>
2021-06-21 14:40:11 +0800

    Update diy-part.sh

--- build/Lede_source/diy-part.sh
+++ build/Lede_source/diy-part.sh
@@ -13,6 +13,9 @@ sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenWrt'' packa
 # 版本号里显示一个自己的名字（MCydia build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
 sed -i "s/OpenWrt /MCydia Compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 
+# 关闭IPv6 分配长度
+sed -i '/ip6assign/d' package/base-files/files/bin/config_generate
+
 # 关闭DHCP服务
 echo "close_dhcp" > package/base-files/files/etc/closedhcp                                                 
 
