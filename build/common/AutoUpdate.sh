f93bc3595c8a0cae5ac562bdebc190da1bb9495a
MCydia <61176750+MCydia@users.noreply.github.com>
2021-07-02 00:33:54 +0800

    Update AutoUpdate.sh

--- build/common/AutoUpdate.sh
+++ build/common/AutoUpdate.sh
@@ -3,14 +3,14 @@
 # AutoBuild Module by Hyy2001
 # AutoUpdate for Openwrt
 
-Version=V5.8
+Version=V5.9
 
 Shell_Helper() {
 cat <<EOF
 更新参数:
 		bash /bin/AutoUpdate.sh				[保留配置更新]
 		bash /bin/AutoUpdate.sh	-n			[不保留配置更新]
-		bash /bin/AutoUpdate.sh	-f			[更改其他作者固件，不保留配置更新]
+		bash /bin/AutoUpdate.sh	-g			[更改其他作者固件，不保留配置更新]
 			
 其    他:
 		bash /bin/AutoUpdate.sh	-c			[更换检查更新以及固件下载的Github地址]
@@ -35,7 +35,7 @@ Github 地址:		${Github}
 固件下载地址:		${Github_Release}
 固件作者:		${Author}
 作者仓库:		${CangKu}
-固件名称:		${Firmware_COMP1}-${CURRENT_Version}${Firmware_SFX}
+固件名称:		${LUCI_Name}-${CURRENT_Version}${Firmware_SFX}
 固件格式:		${Firmware_GESHI}
 EOF
 [[ "${DEFAULT_Device}" == x86-64 ]] && {
@@ -47,133 +47,114 @@ EOF
 }
 exit 0
 }
+[ -f /etc/openwrt_info ] && chmod +x /etc/openwrt_info
 [ -f /etc/openwrt_info ] && source /etc/openwrt_info || {
-	TIME r "未检测到 /etc/openwrt_info,无法运行更新程序!"
+	TIME r "未检测到更新插件所需文件,无法运行更新程序!"
 	exit 1
 }
 GengGai_Install() {
+[ ! -d ${Download_Path} ] && mkdir -p ${Download_Path}
+wget -q --timeout 5 ${Github_Tags} -O ${Download_Path}/Github_Tags
+[[ ! $? == 0 ]] && {
+	TIME r "获取固件版本信息失败,请检测网络是否翻墙或更换节点再尝试!"
+	exit 1
+}
 source /etc/openwrt_info
-TIME h "执行：转换其他作者固件操作"
+Kernel="$(egrep -o "[0-9]+\.[0-9]+\.[0-9]+" /usr/lib/opkg/info/kernel.control)"
+case ${DEFAULT_Device} in
+x86-64)
+	[ -f /etc/openwrt_boot ] && {
+		export BOOT_Type="-$(cat /etc/openwrt_boot)"
+	} || {
+		[ -d /sys/firmware/efi ] && {
+			export BOOT_Type="-UEFI"
+		} || export BOOT_Type="-Legacy"
+	}
+	[[ "${Firmware_Type}" == img.gz ]] && {
+		export Firmware_SFX="${BOOT_Type}.img.gz"
+	} || {
+		export Firmware_SFX="${BOOT_Type}.img"
+	}
+;;
+*)
+	export Firmware_SFX=".${Firmware_Type}"
+esac
+TIME h "执行：转换成其他源码固件"
 echo
-TIME y "执行前提是你编译了有其他作者的固件且已经发布到github的releases里的update_Firmware"
+if [[ "${REPO_Name}" == "lede" ]]; then
+	lienol_Device="19.07-lienol-${DEFAULT_Device}.*${Firmware_SFX}"
+	if [[ `cat ${Download_Path}/Github_Tags | grep -c "${lienol_Device}"` -ge '1' ]]; then
+		TIME z "请注意：选择更改其他源码固件后立即执行不保留配置安装固件"
+		TIME y "您当前固件为：${REPO_Name} ${Luci_Edition} ${Kernel} 内核版!"
+	else
+		TIME r "没有检测到有其他作者相同机型的固件版本或者固件格式不相同!"
+		echo
+		exit 1
+	fi
+fi
+if [[ "${REPO_Name}" == "lienol" ]]; then
+	lede_Device="18.06-lede-${DEFAULT_Device}.*${Firmware_SFX}"
+	if [[ `cat ${Download_Path}/Github_Tags | grep -c "${lede_Device}"` -ge '1' ]]; then
+		TIME z "请注意：选择更改其他源码固件后立即执行不保留配置安装固件"
+		TIME y "您当前固件为：${REPO_Name} ${Luci_Edition} ${Kernel} 内核版!"
+	else
+		TIME r "没有检测到有其他作者相同机型的固件版本或者固件格式不相同!"
+		echo
+		exit 1
+	fi
+fi
 echo
-TIME r "警告：选择[ 1、2、3、4 ]后，会立即执行强制不保留配置升级!"
 echo
-echo
-TIME b1 " 1. Lede 18.06 5.4内核版本 固件 "
-echo
-TIME b1 " 2. Lienol 19.07 4.14内核版本 固件"
-echo
-TIME b1 " 3. Project 18.06 4.19内核版本 固件 "
-echo
-TIME b1 " 4. Spirit 21.02 5.4内核版本 固件 "
-echo
-TIME r " 5. 退出固件转换程序"
-echo
-echo
+if [[ "${REPO_Name}" == "lede" ]]; then
+	TIME B " 1. 转换成 Lienol 19.07 其他内核版本?"
+	Luci_Edition="19.07"
+	CURRENT_Version="lienol-${DEFAULT_Device}-202106010101"
+	Egrep_Firmware="19.07-lienol-${DEFAULT_Device}"
+	LUCI_Name="19.07"
+	REPO_Name="lienol"
 
+else
+	TIME B " 1. 转换成 Lede 18.06 其他内核版本?"
+	Luci_Edition="18.06"
+	CURRENT_Version="lede-${DEFAULT_Device}-202106010101"
+	Egrep_Firmware="18.06-lede-${DEFAULT_Device}"
+	LUCI_Name="18.06"
+	REPO_Name="lede"
+
+fi
+echo
+TIME B " 2. 退出固件转换程序?"
+echo
+echo
 while :; do
-
-TIME g "请选序列号[ 1、2、3、4、5 ]输入，然后回车确认您的选择！"
+TIME g "请选序列号[ 1、2 ]输入，然后回车确认您的选择！"
 echo
 read -p "输入您的选择： " CHOOSE
-
 case $CHOOSE in
 	1)
-		echo "
-		Github="${Github}"
-		Author="${Author}"
-		CangKu="${CangKu}"
-		Luci_Edition="18.06"
-		CURRENT_Version="lede-${DEFAULT_Device}-202106010101"
-		DEFAULT_Device="${DEFAULT_Device}"
-		Firmware_Type="${Firmware_Type}"
-		Firmware_COMP1="coolsnowwolf-18.06"
-		Firmware_COMP2="lede"
-		Github_Release="${Github_Release}"
-		Github_Tags="${Github_Tags}"
-		XiaZai="${XiaZai}"
-		" > /etc/openwrt_info
-		awk '{sub(/^[ \t]+/,"");print $0}' /etc/openwrt_info > /etc/opgenggai
-		cat /etc/opgenggai > /etc/openwrt_info && rm -rf opgenggai
-		Upgrade_Options="${Github_Tags}"
-		TIME y "您已把固件更改为[ Lede 18.06 5.4内核版本 ]！"
-		TIME y "开始升级固件,请稍后...！"
+		cat >/etc/openwrt_info <<-EOF
+		Github=${Github}
+		Author=${Author}
+		CangKu=${CangKu}
+		Luci_Edition=${Luci_Edition}
+		CURRENT_Version=${CURRENT_Version}
+		DEFAULT_Device=${DEFAULT_Device}
+		Firmware_Type=${Firmware_Type}
+		LUCI_Name=${LUCI_Name}
+		REPO_Name=${REPO_Name}
+		Github_Release=${Github_Release}
+		Github_Tags=${Github_Tags}
+		Egrep_Firmware=${Egrep_Firmware}
+		Download_Path=${Download_Path}
+		EOF
+		TIME y "转换固件成功，开始安装新源码的固件,请稍后...！"
 		sleep 5
-		bash /bin/AutoUpdate.sh -s
+		bash /bin/AutoUpdate.sh	-s
 	break
 	;;
 	2)
-		echo "
-		Github="${Github}"
-		Author="${Author}"
-		CangKu="${CangKu}"
-		Luci_Edition="19.07"
-		CURRENT_Version="lienol-${DEFAULT_Device}-202106010101"
-		DEFAULT_Device="${DEFAULT_Device}"
-		Firmware_Type="${Firmware_Type}"
-		Firmware_COMP1="openwrt-19.07"
-		Firmware_COMP2="lienol"
-		Github_Release="${Github_Release}"
-		Github_Tags="${Github_Tags}"
-		XiaZai="${XiaZai}"
-		" > /etc/openwrt_info
-		awk '{sub(/^[ \t]+/,"");print $0}' /etc/openwrt_info > /etc/opgenggai
-		cat /etc/opgenggai > /etc/openwrt_info && rm -rf opgenggai
-		TIME y "您已把固件更改为[ Lienol 19.07 4.14内核版本 ]！"
-		TIME y "开始升级固件,请稍后...！"
-		sleep 5
-		bash /bin/AutoUpdate.sh -s
-	break
-	;;
-	3)
-		echo "
-		Github="${Github}"
-		Author="${Author}"
-		CangKu="${CangKu}"
-		Luci_Edition="18.06"
-		CURRENT_Version="project-${DEFAULT_Device}-202106010101"
-		DEFAULT_Device="${DEFAULT_Device}"
-		Firmware_Type="${Firmware_Type}"
-		Firmware_COMP1="immortalwrt-18.06"
-		Firmware_COMP2="project"
-		Github_Release="${Github_Release}"
-		Github_Tags="${Github_Tags}"
-		XiaZai="${XiaZai}"
-		" > /etc/openwrt_info
-		awk '{sub(/^[ \t]+/,"");print $0}' /etc/openwrt_info > /etc/opgenggai
-		cat /etc/opgenggai > /etc/openwrt_info && rm -rf opgenggai
-		TIME y "您已把固件更改为[ Project 18.06 4.19内核版本 ]！"
-		TIME y "开始升级固件,请稍后...！"
-		sleep 5
-		bash /bin/AutoUpdate.sh -s
-	break
-	;;
-	4)
-		echo "
-		Github="${Github}"
-		Author="${Author}"
-		CangKu="${CangKu}"
-		Luci_Edition="21.02"
-		CURRENT_Version="Spirit-${DEFAULT_Device}-202106010101"
-		DEFAULT_Device="${DEFAULT_Device}"
-		Firmware_Type="${Firmware_Type}"
-		Firmware_COMP1="ctcgfw-21.02"
-		Firmware_COMP2="Spirit"
-		Github_Release="${Github_Release}"
-		Github_Tags="${Github_Tags}"
-		XiaZai="${XiaZai}"
-		" > /etc/openwrt_info
-		awk '{sub(/^[ \t]+/,"");print $0}' /etc/openwrt_info > /etc/opgenggai
-		cat /etc/opgenggai > /etc/openwrt_info && rm -rf opgenggai
-		TIME y "您已把固件更改为[ Spirit 21.02 5.4内核版本 ]！"
-		TIME y "开始升级固件,请稍后...！"
-		sleep 5
-		bash /bin/AutoUpdate.sh -s
-	break
-	;;
-	5)
 		TIME r "您退出了固件转换程序"
+		echo
 		sleep 2
 		exit 0
 	;;
@@ -182,10 +163,9 @@ done
 }
 export Input_Option=$1
 export Input_Other=$2
-export Download_Path="/tmp/Downloads"
 export Overlay_Available="$(df -h | grep ":/overlay" | awk '{print $4}' | awk 'NR==1')"
 rm -rf "${Download_Path}" && TMP_Available="$(df -m | grep "/tmp" | awk '{print $4}' | awk 'NR==1' | awk -F. '{print $1}')"
-[ ! -d "${Download_Path}" ] && mkdir -p "${Download_Path}"
+[ ! -d "${Download_Path}" ] && mkdir -p ${Download_Path}
 opkg list | awk '{print $1}' > ${Download_Path}/Installed_PKG_List
 TIME() {
 	[ ! -f /tmp/AutoUpdate.log ] && touch ${Download_Path}/AutoUpdate.log
@@ -196,7 +176,7 @@ TIME() {
 		r) export Color="\e[31;1m";;
 		g) export Color="\e[32m";;
 		b) export Color="\e[34m";;
-		b1) export Color="\e[34;1m";;
+		B) export Color="\e[34;1m";;
 		y) export Color="\e[33m";;
 		z) export Color="\e[35;1m";;
 		h) export Color="\e[36;1m";;
@@ -234,24 +214,20 @@ x86-64)
 	export CURRENT_Des="$(jsonfilter -e '@.model.id' < /etc/board.json | tr ',' '_')"
 	export CURRENT_Device="${CURRENT_Des} (x86-64)"
   	export Firmware_SFX="${BOOT_Type}.${Firmware_Type}"
-	export Firmware_TAR="${BOOT_Type}.tar.gz"
   	export Firmware_GESHI=".${Firmware_Type}"
-	export Detail_SFX="${BOOT_Type}.detail"
 ;;
 *)
 	export CURRENT_Device="$(jsonfilter -e '@.model.id' < /etc/board.json | tr ',' '_')"
 	export Firmware_SFX=".${Firmware_Type}"
-	export Firmware_TAR=".tar.gz"
   	export Firmware_GESHI=".${Firmware_Type}"
 	[[ -z ${Firmware_SFX} ]] && export Firmware_SFX=".bin"
-	export Detail_SFX=".detail"
 esac
 CURRENT_Ver="${CURRENT_Version}${BOOT_Type}"
 cd /etc
 clear && echo "Openwrt-AutoUpdate Script ${Version}"
 echo
 if [[ -z "${Input_Option}" ]];then
-	export Upgrade_Options="-c"
+	export Upgrade_Options="-v"
 	TIME g "执行: 保留配置更新固件[静默模式]"
 else
 	case ${Input_Option} in
@@ -275,7 +251,7 @@ else
 
 		-u)
 			export AutoUpdate_Mode=1
-			export Upgrade_Options="-c"
+			export Upgrade_Options="-v"
 		;;
 		esac
 	;;
@@ -314,36 +290,50 @@ else
 	-h | -help)
 		Shell_Helper
 	;;
-	-f)
+	-g)
 		GengGai_Install	
 	;;
 	-b)
 		TIME h "执行：引导格式更改操作"
 		echo
 		TIME r "警告：更改引导格式有更新固件时不能安装固件的风险,请慎重！"
-		TIME h "爱快虚拟机的请勿使用,因爱快虚拟机只支持Legacy引导格式"
-		TIME z "请注意：选择更改引导模式后会立即执行不保留配置升级固件"
-		TIME y "当前引导模式为:${EFI_Mode}"
+		TIME h "爱快虚拟机的请勿使用,因爱快虚拟机只支持Legacy引导格式!"
+		TIME z "请注意：选择更改引导模式后会立即执行不保留配置升级固件!"
+		[ -f /etc/openwrt_boot ] && {
+			export x86_64_Boot="$(cat /etc/openwrt_boot)"
+			TIME y "您现在的引导模式为：${x86_64_Boot}"
+		} || {
+			[ -d /sys/firmware/efi ] && {
+				export x86_64_Boot="UEFI"
+				TIME y "您现在的引导模式为：${x86_64_Boot}"
+			} || export x86_64_Boot="Legacy"
+			TIME y "您现在的引导模式为：${x86_64_Boot}"
+		}
 		echo
 		echo
-		TIME b1 " 1. 强制改为[Legacy引导格式]"
-		TIME b1 " 2. 强制改为[UEFI引导格式]"
-		TIME r " 3. 退出引导更改程序"
+		[[ "${x86_64_Boot}" == "UEFI" ]] && {
+			TIME B " 1. 强制改为[Legacy引导格式]?"
+			EFI_Mode="Legacy"
+		} || {
+			TIME B " 1. 强制改为[UEFI引导格式]?"
+			EFI_Mode="UEFI"
+		}
+		TIME B " 2. 退出引导更改程序?"
 		echo
 		echo
 		while :; do
-		TIME g "请选择序列号[ 1、2、3 ]输入,然后回车确认您的选择！"
+		TIME g "请选择序列号[ 1、2 ]输入,然后回车确认您的选择！"
 		echo
 		read -p "请输入您的选择： " YDGS
 		case $YDGS in
 			1)
 				source /etc/openwrt_info
-				echo "Legacy" > /etc/openwrt_boot
+				echo "${EFI_Mode}" > /etc/openwrt_boot
 				sed -i '/openwrt_boot/d' /etc/sysupgrade.conf
 				echo -e "\n/etc/openwrt_boot" >> /etc/sysupgrade.conf
-				TIME y "固件引导方式已指定为: Legacy!"
+				TIME y "固件引导方式已指定为: ${EFI_Mode}!"
 				sed -i '/CURRENT_Version/d' /etc/openwrt_info > /dev/null 2>&1
-				echo -e "\nCURRENT_Version=${Firmware_COMP2}-${DEFAULT_Device}-202106010101" >> /etc/openwrt_info
+				echo -e "\nCURRENT_Version=${REPO_Name}-${DEFAULT_Device}-202106010101" >> /etc/openwrt_info
 				TIME y "3秒后开始更新固件，请稍后...!"
 				echo
 				sleep 3
@@ -351,20 +341,6 @@ else
 			break
 			;;
 			2)
-				source /etc/openwrt_info
-				echo "UEFI" > /etc/openwrt_boot
-				sed -i '/openwrt_boot/d' /etc/sysupgrade.conf
-				echo -e "\n/etc/openwrt_boot" >> /etc/sysupgrade.conf
-				TIME y "固件引导方式已指定为: Legacy!"
-				sed -i '/CURRENT_Version/d' /etc/openwrt_info > /dev/null 2>&1
-				echo -e "\nCURRENT_Version=${Firmware_COMP2}-${DEFAULT_Device}-202106010101" >> /etc/openwrt_info
-				TIME y "3秒后开始更新固件，请稍后...!"
-				echo
-				sleep 3
-				bash /bin/AutoUpdate.sh -s
-			break
-			;;
-			3)
 				TIME r "您选择了退出更改程序"
 				echo
 				exit 0
@@ -383,49 +359,40 @@ if [[ "$(cat ${Download_Path}/Installed_PKG_List)" =~ curl ]];then
 	export Google_Check=$(curl -I -s --connect-timeout 8 google.com -w %{http_code} | tail -n1)
 	if [ ! "$Google_Check" == 301 ];then
 		TIME z "网络检测失败,因Github现在也筑墙了,请先使用梯子翻墙再来尝试!"
+		sleep 2
 		exit 1
 	else
 		TIME y "网络检测成功,您的梯子翻墙成功！"
 	fi
 fi
-if [[ -z "${CURRENT_Version}" ]];then
-	TIME r "警告: 本地固件版本获取失败,请检查/etc/openwrt_info文件的CURRENT_Version值!"
-	exit 1
-fi
-if [[ -z "${CURRENT_Device}" ]];then
-	[[ -n "$DEFAULT_Device" ]] && {
-		TIME z "警告: 当前设备名称获取失败,使用预设名称: [$DEFAULT_Device]"
-		export CURRENT_Device="${DEFAULT_Device}"
-	} || {
-		TIME r "未检测到设备名称,无法执行更新!"
-		exit 1
-	}
-fi
+[[ -z ${CURRENT_Version} ]] && TIME r "本地固件版本获取失败,请检查/etc/openwrt_info文件的值!" && EXIT 1
+[[ -z ${Github} ]] && TIME r "Github地址获取失败,请检查/etc/openwrt_info文件的值!" && EXIT 1
+[[ -z ${Github_Release} ]] && TIME r "固件下载地址获取失败,请检查/etc/openwrt_info文件的值!" && EXIT 1
+[[ -z ${Github_Tags} ]] && TIME r "解析 API 地址获取失败,请检查/etc/openwrt_info文件的值!" && EXIT 1
+[[ -z ${Firmware_Type} ]] && TIME r "固件后缀获取失败,请检查/etc/openwrt_info文件的值!" && EXIT 1
 TIME g "正在获取固件版本信息..."
-wget -q --timeout 5 ${Github_Tags} -O - > ${Download_Path}/Github_Tags
+[ ! -d ${Download_Path} ] && mkdir -p ${Download_Path}
+wget -q --timeout 5 ${Github_Tags} -O ${Download_Path}/Github_Tags
 [[ ! $? == 0 ]] && {
-	TIME r "获取固件版本信息失败,请稍后重试!"
+	TIME r "获取固件版本信息失败,请检测网络是否翻墙或更换节点再尝试!"
 	exit 1
 }
 TIME g "正在比对云端固件和本地安装固件版本..."
-export CLOUD_Firmware="$(egrep -o "${Firmware_COMP1}-${Firmware_COMP2}-${DEFAULT_Device}-[a-zA-Z0-9_-]+.*?[0-9]+${Firmware_TAR}" ${Download_Path}/Github_Tags | awk 'END {print}')"
-export CLOUD_Version="$(echo ${CLOUD_Firmware} | egrep -o "${Firmware_COMP2}-${DEFAULT_Device}-[a-zA-Z0-9_-]+.*?[0-9]+${BOOT_Type}")"
+export CLOUD_Firmware="$(egrep -o "${Egrep_Firmware}-[0-9]+${Firmware_SFX}" ${Download_Path}/Github_Tags | awk 'END {print}')"
+export CLOUD_Version="$(echo ${CLOUD_Firmware} | egrep -o "${REPO_Name}-${DEFAULT_Device}-[0-9]+${BOOT_Type}")"
 [[ -z "${CLOUD_Version}" ]] && {
 	TIME r "比对固件版本失败!"
 	exit 1
 }
-export Firmware_Name="$(echo ${CLOUD_Firmware} | egrep -o "${Firmware_COMP1}-${Firmware_COMP2}-${DEFAULT_Device}-[a-zA-Z0-9_-]+.*?[0-9]+")"
-export Firmware="${Firmware_Name}${Firmware_SFX}"
-export Firmware_Detail="${Firmware_Name}${Detail_SFX}"
-export Firmware_TARGZ="${Firmware_Name}${Firmware_TAR}"
-let X=$(grep -n "${Firmware_TARGZ}" ${Download_Path}/Github_Tags | tail -1 | cut -d : -f 1)-4
-let Y=$(sed -n "${X}p" ${Download_Path}/Github_Tags | egrep -o "[0-9]+" | awk '{print ($1)/1048576}' | awk -F. '{print $1}')+1
-export CLOUD_Firmware_Size="$(($Y+$Y))"
+export Firmware_Name="$(echo ${CLOUD_Firmware} | egrep -o "${Egrep_Firmware}-[0-9]+")"
+export Firmware="${CLOUD_Firmware}"
+let X=$(grep -n "${Firmware}" ${Download_Path}/Github_Tags | tail -1 | cut -d : -f 1)-4
+let CLOUD_Firmware_Size=$(sed -n "${X}p" ${Download_Path}/Github_Tags | egrep -o "[0-9]+" | awk '{print ($1)/1048576}' | awk -F. '{print $1}')+1
 echo -e "\n本地版本：${CURRENT_Ver}"
 echo "云端版本：${CLOUD_Version}"	
 [[ "${TMP_Available}" -lt "${CLOUD_Firmware_Size}" ]] && {
 	TIME g "tmp 剩余空间: ${TMP_Available}M"
-	TIME r "tmp空间不足[${CLOUD_Firmware_Size}M],不够下载和解压固件所需,请清理tmp空间或者增加运行内存!"
+	TIME r "tmp空间不足[${CLOUD_Firmware_Size}M],不够下载固件所需,请清理tmp空间或者增加运行内存!"
 	echo
 	exit 1
 }
@@ -465,51 +432,23 @@ echo "固件格式：${Firmware_GESHI}"
 	echo "引导模式：${EFI_Mode}"
 }
 echo "固件名称：${Firmware}"
-echo "云压缩包：${Firmware_TARGZ}"
 echo "下载保存：${Download_Path}"
 sleep 1
 cd ${Download_Path}
 TIME g "正在下载云端固件,请耐心等待..."
-wget -q --no-cookie --no-check-certificate -T 15 -t 4 "https://download.fastgit.org/${XiaZai}/${Firmware_TARGZ}" -O ${Firmware_TARGZ}
+wget -q --no-cookie --no-check-certificate -T 15 -t 4 "${Github_Release}/${Firmware}" -O ${Firmware}
 if [[ $? -ne 0 ]];then
-	TIME z "FastGit下载云端固件失败,切换下载模式!"
-	TIME b1 "切换普通模式继续下载固件,请耐心等待......"
-	wget -q --no-cookie --no-check-certificate -T 15 -t 4 "https://github.com/${XiaZai}/${Firmware_TARGZ}" -O ${Firmware_TARGZ}
+	wget -q --no-cookie --no-check-certificate -T 15 -t 4 "https://ghproxy.com/${Github_Release}/${Firmware}" -O ${Firmware}
 	if [[ $? -ne 0 ]];then
 		TIME r "下载云端固件失败,请尝试手动安装!"
+		echo
 		exit 1
 	else
-		TIME y "使用普通模式下载云端固件成功!"
+		TIME y "下载云端固件成功!"
 	fi
 else
-	TIME y "使用FastGit下载云端固件成功!"
+	TIME y "下载云端固件成功!"
 fi
-tar zxf ${Firmware_TARGZ} && rm -rf ${Firmware_TARGZ}
-mv -f GDfirmware/* ./ && rm -rf GDfirmware
-CLOUD_MD5="$(awk -F '[ :]' '/MD5/ {print $2;exit}' ${Firmware_Detail})"
-CURRENT_MD5="$(md5sum ${Firmware} | cut -d ' ' -f1)"
-[[ -z "${CLOUD_MD5}" ]] || [[ -z "${CURRENT_MD5}" ]] && {
-	TIME r "在Detail文件获取 MD5 失败,请检查Detail文件里是否有MD5数值!"
-	exit 1
-}
-CLOUD_SHA256="$(awk -F '[ :]' '/SHA256/ {print $2;exit}' ${Firmware_Detail})"
-CURRENT_SHA256="$(sha256sum ${Firmware} | cut -d ' ' -f1)"
-[[ -z "${CLOUD_SHA256}" ]] || [[ -z "${CURRENT_SHA256}" ]] && {
-	TIME r "在Detail文件获取 SHA256 失败,请检查Detail文件里是否有SHA256数值!"
-	exit 1
-}
-[[ "${CURRENT_MD5}" != "${CLOUD_MD5}" ]] && {
-	echo -e "\n本地MD5: ${CURRENT_MD5}"
-	echo "云端MD5: ${CLOUD_MD5}"
-	TIME r "MD5 对比失败,固件可能因网络原因固件下载不完整"
-	exit 1
-}
-[[ "${CURRENT_SHA256}" != "${CLOUD_SHA256}" ]] && {
-	echo -e "\n本地SHA256: ${CURRENT_SHA256}"
-	echo "云端SHA256: ${CLOUD_SHA256}"
-	TIME r "SHA256 对比失败,固件可能因网络原因固件下载不完整"
-	exit 1
-}
 TIME g "准备就绪,开始刷写固件..."
 [[ "${Input_Other}" == "-t" ]] && {
 	TIME z "测试模式运行完毕!"
@@ -518,7 +457,10 @@ TIME g "准备就绪,开始刷写固件..."
 	echo
 	exit 0
 }
-TIME h "3秒后开始刷写固件,可能需要2-3分钟,期间请耐心等待..."
-sleep 3
+
+chmod 777 ${Firmware}
 sysupgrade ${Upgrade_Options} ${Firmware}
-exit 0
+[[ $? -ne 0 ]] && {
+	TIME r "固件刷写失败,请尝试手动更新固件!"
+	exit 1
+} || exit 0
