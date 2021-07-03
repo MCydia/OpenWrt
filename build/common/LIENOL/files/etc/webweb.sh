4a4ff26df35ead26625c863529d5610d28e116cd
MCydia <61176750+MCydia@users.noreply.github.com>
2021-07-03 21:17:58 +0800

    Update webweb.sh

--- build/common/LIENOL/files/etc/webweb.sh
+++ build/common/LIENOL/files/etc/webweb.sh
@@ -6,6 +6,8 @@ if [ -n "$(ls -A "/etc/closedhcp" 2>/dev/null)" ]; then
   sed -i '/leasetime/d' /etc/config/dhcp
   rm -rf /etc/closedhcp
 fi
+source /etc/openwrt_release
+sed -i "s/x86_64/${DISTRIB_TARGET}/g" /etc/banner
 sed -i 's/<%=pcdata(ver.distversion)%>/<%=pcdata(ver.distversion)%><!--/g' /usr/lib/lua/luci/view/admin_status/index.htm
 sed -i 's/(<%=pcdata(ver.luciversion)%>)/(<%=pcdata(ver.luciversion)%>)-->/g' /usr/lib/lua/luci/view/admin_status/index.htm
 sed -i '/coremark.sh/d' /etc/crontabs/root
