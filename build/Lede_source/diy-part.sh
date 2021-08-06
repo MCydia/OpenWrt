ee963b982899ab1688a654339974b3fff242b75e
skycn3322 <34830820+skycn3322@users.noreply.github.com>
2021-08-06 13:52:44 +0800

    Update diy-part.sh

--- build/Lede_source/diy-part.sh
+++ build/Lede_source/diy-part.sh
@@ -61,19 +61,19 @@ sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ
 # rm -rf ./feeds/luci/applications/luci-app-upnp          # 删除upnp自动端口映射
 # rm -rf ./feeds/luci/applications/luci-app-wol           # 删除wol网络唤醒
 # rm -rf ./feeds/luci/applications/luci-app-nlbwmon     # 删除nlbwmon流量监控
-rm -rf ./package/lean/luci-app-pptp-server              # 删除PPTP-VPN服务端
-rm -rf ./package/lean/luci-app-qbittorrent              # 删除qbittorrent
-rm -rf ./package/lean/qBittorrent-static                # 删除qbittorrent
-rm -rf ./package/lean/luci-app-qbittorrent_static       # 删除qbittorrent
-rm -rf ./feeds/packages/net/zerotier                    # 删除zerotier内网穿透
-rm -rf ./package/lean/luci-app-zerotier                 # 删除zerotier内网穿透
-rm -rf ./package/lean/luci-app-unblockmusic             # 删除网易云音乐解锁
-rm -rf ./package/lean/UnblockNeteaseMusic-Go            # 删除网易云音乐解锁
-rm -rf ./package/lean/adbyby                            # 删除广告大师
-rm -rf ./package/lean/luci-app-adbyby-plus              # 删除广告大师
-rm -rf ./package/lean/luci-app-xlnetacc                 # 删除迅雷快鸟
-rm -rf ./package/lean/uugamebooster                     # 删除UU游戏加速
-rm -rf ./package/lean/luci-app-uugamebooster            # 删除UU游戏加速
+# rm -rf ./package/lean/luci-app-pptp-server              # 删除PPTP-VPN服务端
+# rm -rf ./package/lean/luci-app-qbittorrent              # 删除qbittorrent
+# rm -rf ./package/lean/qBittorrent-static                # 删除qbittorrent
+# rm -rf ./package/lean/luci-app-qbittorrent_static       # 删除qbittorrent
+# rm -rf ./feeds/packages/net/zerotier                    # 删除zerotier内网穿透
+# rm -rf ./package/lean/luci-app-zerotier                 # 删除zerotier内网穿透
+# rm -rf ./package/lean/luci-app-unblockmusic             # 删除网易云音乐解锁
+# rm -rf ./package/lean/UnblockNeteaseMusic-Go            # 删除网易云音乐解锁
+# rm -rf ./package/lean/adbyby                            # 删除广告大师
+# rm -rf ./package/lean/luci-app-adbyby-plus              # 删除广告大师
+# rm -rf ./package/lean/luci-app-xlnetacc                 # 删除迅雷快鸟
+# rm -rf ./package/lean/uugamebooster                     # 删除UU游戏加速
+# rm -rf ./package/lean/luci-app-uugamebooster            # 删除UU游戏加速
 rm -rf ./feeds/luci/applications/luci-app-adguardhome
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/adguarehome
 # 自定义插件
