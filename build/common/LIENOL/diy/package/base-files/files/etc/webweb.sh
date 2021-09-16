f692bd1e55d5187e3a5d154210dc73dacf59d19f
MCydia <61176750+MCydia@users.noreply.github.com>
2021-09-16 14:59:11 +0800

    Update webweb.sh

--- build/common/LIENOL/diy/package/base-files/files/etc/webweb.sh
+++ build/common/LIENOL/diy/package/base-files/files/etc/webweb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sed -i '/DISTRIB_REVISION/d' /etc/openwrt_release
-echo "DISTRIB_REVISION='19.07'" >> /etc/openwrt_release
+echo "DISTRIB_REVISION='Lienol-19.07'" >> /etc/openwrt_release
 
 sed -i 's/<%=pcdata(ver.distversion)%>/<%=pcdata(ver.distversion)%><!--/g' /usr/lib/lua/luci/view/admin_status/index.htm
 sed -i 's/(<%=pcdata(ver.luciversion)%>)/(<%=pcdata(ver.luciversion)%>)-->/g' /usr/lib/lua/luci/view/admin_status/index.htm
@@ -13,6 +13,8 @@ sed -i 's/luciversion %>)<\/a> \//luciversion %>)<\/a> \/-->/g' /usr/lib/lua/luc
 
 cp -Rf /etc/config/network /mnt/network
 
+echo "0 2 1 * * rm /tmp/luci-indexcache > /dev/null 2>&1" >> /etc/crontabs/root
+
 if [[ `grep -c "x86_64" /etc/openwrt_release` -eq '0' ]]; then
   source /etc/openwrt_release
   sed -i "s/x86_64/${DISTRIB_TARGET}/g" /etc/banner
@@ -23,8 +25,6 @@ if [[ -e /usr/share/AdGuardHome ]] && [[ -e /etc/init.d/AdGuardHome ]]; then
 fi
 
 chmod -R +x /etc/init.d /usr/share
-
-sed -i '/coremark/d' /etc/crontabs/root
 
 rm -rf /etc/networkip
 rm -rf /etc/webweb.sh
