4c04fdee0ad366d7840b24baea951d9e0b5be9e4
MCydia <61176750+MCydia@users.noreply.github.com>
2021-12-23 10:52:15 +0800

    Update webweb.sh

--- build/common/LEDE/diy/package/base-files/files/etc/webweb.sh
+++ build/common/LEDE/diy/package/base-files/files/etc/webweb.sh
@@ -2,12 +2,13 @@
 
 sed -i 's/<a href="https:\/\/github/<!--<a href="https:\/\/github/g' /usr/lib/lua/luci/view/themes/*/footer.htm
 sed -i 's/luciversion %>)<\/a> \//luciversion %>)<\/a> \/-->/g' /usr/lib/lua/luci/view/themes/*/footer.htm
+sed -i 's/distversion %><\/a> \//distversion %><\/a> \/-->/g' /usr/lib/lua/luci/view/themes/*/footer.htm
 
 [[ ! -f /mnt/network ]] && chmod +x /etc/networkip && source /etc/networkip
 
 cp -Rf /etc/config/network /mnt/network
 
-echo "0 2 1 * * rm /tmp/luci-indexcache > /dev/null 2>&1" >> /etc/crontabs/root
+echo "0 1 * * 1 rm /tmp/luci-indexcache > /dev/null 2>&1" >> /etc/crontabs/root
 
 if [[ `grep -c "x86_64" /etc/openwrt_release` -eq '0' ]]; then
   source /etc/openwrt_release
@@ -16,6 +17,16 @@ fi
 
 if [[ -e /usr/share/AdGuardHome ]] && [[ -e /etc/init.d/AdGuardHome ]]; then
  chmod -R +x /usr/share/AdGuardHome /etc/init.d/AdGuardHome
+fi
+
+if [[ ! -e /usr/bin/AdGuardHome ]]; then
+rm -fr /etc/config/AdGuardHome.yaml
+rm -fr /etc/AdGuardHome.yaml
+fi
+
+if [[ -e /etc/init.d/ddnsto ]]; then
+ chmod +x /etc/init.d/ddnsto
+ /etc/init.d/ddnsto enable
 fi
 
 chmod -R +x /etc/init.d /usr/share
